ceeaa2c3e5e34e3ec982acf6d1653dbdd7288dc5
Parth Deshmukh <parthd7122@gmail.com>
2026-03-04 20:07:11 +0000

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { TaskProvider } from "@/contexts/TaskContext";
+import Index from "./pages/Index";
+import CalendarPage from "./pages/CalendarPage";
+import TasksPage from "./pages/TasksPage";
+import NotificationsPage from "./pages/NotificationsPage";
+import LoginPage from "./pages/LoginPage";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <TaskProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/calendar" element={<CalendarPage />} />
+            <Route path="/tasks" element={<TasksPage />} />
+            <Route path="/notifications" element={<NotificationsPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TaskProvider>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
